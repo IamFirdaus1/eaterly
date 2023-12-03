5b55a756cbb479f60ffcabaf93743f1a3386a7aa
Joseph <josephtedjanw@gmail.com>
2023-12-03 13:10:04 +0700

    Optimize login logic, auto login when acc present

--- app/src/main/java/com/dausinvestama/eaterly/SignInActivity.kt
+++ app/src/main/java/com/dausinvestama/eaterly/SignInActivity.kt
@@ -1,13 +1,12 @@
 package com.dausinvestama.eaterly
 
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.ServiceWorkerClient
-import android.widget.Button
+import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import com.dausinvestama.eaterly.databinding.ActivitySignInBinding
 import com.dausinvestama.eaterly.utils.SharedPreferences
@@ -15,18 +14,18 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivitySignInBinding
     private lateinit var googleSigninClient: GoogleSignInClient
-    lateinit var pre: SharedPreferences
+    private lateinit var pre: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,58 +41,89 @@ class SignInActivity : AppCompatActivity() {
 
         googleSigninClient = GoogleSignIn.getClient(this, gso)
 
-        binding.btnlogin.setOnClickListener {
-            signInGoogle()
-        }
+        binding.apply {
 
+            btnGoogleLogin.setOnClickListener {
+                signInGoogle()
+            }
+
+            btnLogin.setOnClickListener {
+                firebaseAuthWithEmailAndPassword(edtEmail.text.toString(), edtPass.text.toString())
+            }
+
+        }
     }
 
-    private fun signInGoogle(){
+    private fun signInGoogle() {
         val signInIntent = googleSigninClient.signInIntent
         launcher.launch(signInIntent)
     }
 
-    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-        result ->
-        if (result.resultCode == Activity.RESULT_OK){
+    private val launcher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-            handleResult(task)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
         }
     }
 
-    private fun handleResult(task: Task<GoogleSignInAccount>){
-        if(task.isSuccessful){
-            val account: GoogleSignInAccount? = task.result
-            if (account != null){
-                updateUI(account)
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
             }
-        }else{
-            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
-        }
     }
 
-    private fun updateUI(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        auth.signInWithCredential(credential).addOnCompleteListener {
-            if (it.isSuccessful){
-                successintent(account)
-            }else{
-                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+    private fun firebaseAuthWithEmailAndPassword(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
             }
-        }
     }
 
     override fun onStart() {
         super.onStart()
 
-        var currentUser = auth.currentUser
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            updateUI(currentUser)
+        }
     }
 
-    private fun successintent(account: GoogleSignInAccount){
-        pre = SharedPreferences(this)
-        val intent= Intent(this, MainActivity::class.java)
-        pre.email = account.email
-        startActivity(intent)
-        finish()
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            pre = SharedPreferences(this)
+            pre.email = currentUser.email
+            startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+            finish()
+        }
     }
 }
